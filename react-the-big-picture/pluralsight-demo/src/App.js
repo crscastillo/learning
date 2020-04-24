7069ac85b501e84948cfadd22ecde35a6e4a877d
crscastillo <crscastillo@gmail.com>
2020-04-24 08:30:58 -0600

    tested addin new h1

--- react-the-big-picture/pluralsight-demo/src/App.js
+++ react-the-big-picture/pluralsight-demo/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Hola Carlos</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>      
     </div>
   );
 }
